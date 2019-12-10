8d67254b4131b9ca623b040e4fab84bd52ca0bee
Ashley Claiborne <languageisaliquid@gmail.com>
2019-12-09 20:19:04 -0600

    printing goats

--- src/components/Goat/Goat.js
+++ src/components/Goat/Goat.js
@@ -1,1 +1,20 @@
 import './Goat.scss';
+import React from 'react';
+
+class Goat extends React.Component {
+  render() {
+    const aGoat = this.props.goat;
+    return (
+      <div className="card col-3 m-2">
+        <img src={aGoat.imgUrl} className="card-img-top" alt={aGoat.name} />
+        <div className="card-body">
+          <h5 className="card-title">{aGoat.name}</h5>
+          <p className="card-text">{aGoat.age}</p>
+          <p className="card-text">{aGoat.description}</p>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Goat;
--- src/components/GoatCoral/GoatCoral.js
+++ src/components/GoatCoral/GoatCoral.js
@@ -1,13 +1,16 @@
 import './GoatCoral.scss';
 import React from 'react';
+import Goat from '../Goat/Goat';
 
 class GoatCoral extends React.Component {
   render() {
     const myGoats = this.props.someGoats;
 
+    const goatCards = myGoats.map((goat) => <Goat key={goat.id} goat={goat} />);
+
     return (
-      <div className='App'>
-        Goat Coral
+      <div className='App row d-flex flex-wrap justify-content-around'>
+        {goatCards}
       </div>
     );
   }
